ff93199e0c6d2fb22165aa51450c71a26160f44f
Brimhesh Tiwari <tiwaribrimhesh@gmail.com>
2025-06-16 00:51:22 +0530

    Serve frontend from backend

--- backend/index.js
+++ backend/index.js
@@ -7,6 +7,7 @@ const cookieParser = require("cookie-parser")
 const workerRoutes = require("./routes/workerRoutes");
 const authRoutes = require("./routes/authRoutes");
 const userRoutes = require("./routes/userRoutes");
+const path = require("path");
 
 dotenv.config();
 
@@ -29,6 +30,12 @@ app.use("/api/auth", authRoutes);
 app.use("/api/user",userRoutes);
 
 const PORT = process.env.PORT || 5000;
+
+app.use(express.static(path.join(__dirname, "dist")));
+
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT,()=>{
  console.log(`server listening on port ${PORT}`)
